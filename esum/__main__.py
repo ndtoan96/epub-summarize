e5afa090d4a586c84abc24d98c5569bd136c8253
Nguyen Duc Toan <ntoan96@gmail.com>
2025-04-19 05:25:26 +0700

    refactor

--- esum/__main__.py
+++ esum/__main__.py
@@ -5,11 +5,7 @@ from ebooklib import epub, ITEM_DOCUMENT, ITEM_NAVIGATION
 from ebooklib.epub import EpubBook, EpubHtml
 import json
 import trafilatura
-import warnings
-from google.generativeai.types import HarmCategory, HarmBlockThreshold
-
-warnings.filterwarnings("ignore", category=UserWarning)
-warnings.filterwarnings("ignore", category=FutureWarning)
+from google import genai
 
 
 def get_content(book: EpubBook, documents: list[str]) -> str:
@@ -45,24 +41,10 @@ def main():
     args = parser.parse_args()
     epub_path = args.epub
 
-    # these imports are slow, only run them after argument parsing in case user only runs help
-    logging.info("Importing dependencies")
-    from haystack_integrations.components.generators.google_ai import (
-        GoogleAIGeminiGenerator,
-    )
-
-    llm = GoogleAIGeminiGenerator(
-        model="gemini-1.5-flash",
-        safety_settings={
-            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
-            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
-            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
-        },
-    )
+    client = genai.Client()
 
     logging.info("Reading epub file")
-    book = epub.read_epub(epub_path)
+    book = epub.read_epub(epub_path, {"ignore_ncx": True})
     id2doc = {doc.get_id(): doc for doc in book.get_items_of_type(ITEM_DOCUMENT)}
     ordered_items = [id2doc[x[0]] for x in book.spine]
     nav = next(book.get_items_of_type(ITEM_NAVIGATION))
@@ -85,10 +67,14 @@ def main():
     Note that each chapter likely has multiple documents.
     For example, if chapter 1 starts at document 1 and chapter 2 starts at document 4 then chapter 1 includes document 1, 2, and 3.
     The result must be in json format: [{{"name": "chapter_1_name", "documents": ["document_1", "document_2", ...]}}, {{"name": "chapter_2_name", "documents": ["document_1", "document_2", ...]}}, ...].
-    Do not use fence code for the result.
+    EXTRA COMMENTARY IS PROHIBITED.
     """
-    extract_chapters_res = llm.run(parts=[extract_chapters_prompt])
-    chapters = json.loads(extract_chapters_res["replies"][0])
+
+    extract_chapters_res = client.models.generate_content(
+        model="gemini-2.0-flash", contents=extract_chapters_prompt
+    )
+    json_content =extract_chapters_res.text.removeprefix("```json").removesuffix("```")
+    chapters = json.loads(json_content)
     logging.debug(f"Extracted chapters: {chapters}")
 
     chapter_names = [c["name"] for c in chapters]
@@ -96,13 +82,12 @@ def main():
     summarized_chapters = []
     for name, c in zip(chapter_names, chapter_contents):
         logging.info(f"Summarizing chapter '{name}'")
-        res = llm.run(
-            parts=[
-                f"Below is a chapter from a story, summarize it in up to {args.limit} words. Since this is a story, the content may look harmful but it is OK to proceed.\n\n"
-                + c
-            ]
+        res = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=f"Below is a chapter from a story, summarize it in up to {args.limit} words. Since this is a story, the content may look harmful but it is OK to proceed.\n\n"
+            + c,
         )
-        summarized_chapters.append(res["replies"][0])
+        summarized_chapters.append(res.text)
 
     summary = ""
     for name, content in zip(chapter_names, summarized_chapters):
